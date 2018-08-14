72a8fbfad8d89dd160c3f1422491363d0a826119
FatLi1989 <514340686@qq.com>
2018-08-14 18:15:59 +0800

    8.14

---
+++ src/common/form/Form.vue
@@ -0,0 +1,100 @@
+<template>
+  <el-container>
+    <el-header v-if="true">
+      <slot name="header"></slot>
+      <el-button type="primary" class="select-button" @click="onSubmit">查询</el-button>
+    </el-header>
+    <el-main>
+      <el-button type="primary" class="add-btn" icon="el-icon-plus" @click="add">新增</el-button>
+      <el-button type="danger" class="del-btn" icon="el-icon-delete" @click="del">删除</el-button>
+      <div class="table">
+        <slot name="body"></slot>
+      </div>
+      <div class="footer">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="[1, 2, 3, 4]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="totalItems">
+        </el-pagination>
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+      name: 'FormDemo'
+    }
+</script>
+
+<style lang="stylus" scoped>
+  .el-header
+    display flex
+    background #fff
+    margin-top 20px
+    margin-left 20px
+    height 100px !important
+    width 95%
+    border-radius 15px
+    box-shadow: 1px 1px 3px rgba(0,0,0,.2)
+    .select-button
+      text-align center
+      position relative
+      height 40px !important
+      bottom -58px !important
+      border-radius 100px
+  .el-main
+    background #fff
+    margin-top 30px
+    margin-left 20px
+    height 75vh
+    width 95%
+    border-radius 15px
+    box-shadow: 1px 1px 3px rgba(0,0,0,.2)
+    /*滚动条样式*/
+    &::-webkit-scrollbar
+      width: 5px
+      height: 10px
+    /*滚动条里面轨道*/
+    &::-webkit-scrollbar-track
+      margin-top 5px
+      margin-bottom 5px
+      box-shadow: 1px 1px 5px rgba(0,0,0,.2) inset
+      border-radius: 10px;
+      background: rgba(0,0,0,0.1);
+    /*滚动条里面小方块*/
+    &::-webkit-scrollbar-thumb
+      border-radius 10px
+      height: 5px
+      background-clip: border-box
+      box-shadow: 0 0 0 5px rgba(0,0,0,.2) inset
+    .add-btn
+      height 40px
+      color  #66b1ff
+      float left
+      border-radius 100px 0px 0px 100px
+      width 100px
+      background whitesmoke
+    .del-btn
+      height 40px
+      color #f56c6c
+      float left
+      margin-left 0px
+      border-radius 0px 100px 100px 0px
+      width 100px
+      background whitesmoke
+    .table
+      margin-top 20px
+  .footer
+    position relative
+    height 40px
+    margin-top 50px
+    text-align right
+    background #fff
+    margin-left 21%
+    width 80%
+</style>
--- src/view/content/user/User.vue
+++ src/view/content/user/User.vue
@@ -1,30 +1,25 @@
 <template>
-  <el-container>
-    <el-header v-if="true">
-      <el-form :inline="true"  class="demo-form-inline" size="mini">
-       <div class="first-line">
-         <el-form-item label="角色名称：">
-           <el-input placeholder="角色名称"></el-input>
-         </el-form-item>
-         <el-form-item label="权限字符：">
-           <el-input placeholder="权限字符"></el-input>
-         </el-form-item>
-         <el-form-item label="角色状态：">
-           <el-select>
-             <el-option label="区域一" value="shanghai"></el-option>
-             <el-option label="区域二" value="beijing"></el-option>
-           </el-select>
-         </el-form-item>
-       </div>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
+  <form-demo>
+    <el-form :inline="true" class="el-form" size="mini" slot="header">
+      <div class="condition-form">
+        <el-form-item label="登录名称：">
+          <el-input placeholder="角色名称"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号码吗：">
+          <el-input placeholder="权限字符"></el-input>
+        </el-form-item>
+        <el-form-item label="用户状态：">
+          <el-select>
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="创建时间：">
+          <calendar></calendar>
+        </el-form-item>
+      </div>
     </el-form>
-    </el-header>
-    <el-main>
-      <el-button type="primary" class="add-btn" icon="el-icon-plus" @click="add">新增</el-button>
-      <el-button type="danger" class="del-btn" icon="el-icon-delete" @click="del">删除</el-button>
-      <div class="table">
+    <div slot="body">
       <el-table
         :data="tableDataBegin"
         style="width: 100%">
@@ -60,22 +55,13 @@
           </template>
         </el-table-column>
       </el-table>
-      </div>
-      <div class="footer">
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="currentPage"
-          :page-sizes="[1, 2, 3, 4]"
-          :page-size="pageSize"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="totalItems">
-        </el-pagination>
-      </div>
-    </el-main>
-  </el-container>
+    </div>
+  </form-demo>
 </template>
 <script>
+  import FormDemo from '../../../common/form/Form.vue'
+  import Calendar from '../../../common/calendar/Calendar.vue'
+
   export default {
     data () {
       return {
@@ -151,18 +137,6 @@
       }
     },
     methods: {
-      add () {
-        alert('add')
-      },
-      del () {
-        alert('del')
-      },
-      handleEdit (index, row) {
-        console.log(index, row);
-      },
-      handleDelete (index, row) {
-        console.log(index, row);
-      },
       /*    前端搜索功能需要区分是否检索,因为对应的字段的索引不同
           用两个变量接收currentChangePage函数的参数 */
       doFilter () {
@@ -215,68 +189,17 @@
           }
         }
       }
-    }
+    },
+    components: { FormDemo, Calendar }
   };
 </script>
 <style lang="stylus" scoped>
-  .el-header
-    background #fff
-    margin-top 20px
-    margin-left 20px
-    width 95%
-    border-radius 15px
-    box-shadow: 1px 1px 3px rgba(0,0,0,.2)
-    .first-line
-     display flex
-     justify-content space-around
-  .el-main
-    background #fff
-    margin-top 30px
-    margin-left 20px
-    height 75vh
-    width 95%
-    border-radius 15px
-    box-shadow: 1px 1px 3px rgba(0,0,0,.2)
-     /*滚动条样式*/
-    &::-webkit-scrollbar
-     width: 5px
-     height: 10px
-    /*滚动条里面轨道*/
-    &::-webkit-scrollbar-track
-      margin-top 5px
-      margin-bottom 5px
-      box-shadow: 1px 1px 5px rgba(0,0,0,.2) inset
-      border-radius: 10px;
-      background: rgba(0,0,0,0.1);
-    /*滚动条里面小方块*/
-    &::-webkit-scrollbar-thumb
-     border-radius 10px
-     height: 5px
-     background-clip: border-box
-     box-shadow: 0 0 0 5px rgba(0,0,0,.2) inset
-    .add-btn
-     height 40px
-     color  #66b1ff
-     float left
-     border-radius 100px 0px 0px 100px
-     width 100px
-     background whitesmoke
-    .del-btn
-      height 40px
-      color #f56c6c
-      float left
-      margin-left 0px
-      border-radius 0px 100px 100px 0px
-      width 100px
-      background whitesmoke
-    .table
-      margin-top 20px
-  .footer
-    position relative
-    height 40px
-    margin-top 50px
-    text-align right
-    background #fff
-    margin-left 21%
-    width 80%
+    .el-form
+     width 90%
+     height 100px !important
+     .condition-form
+      display flex
+      flex-wrap wrap
+      margin-top 10px
+      justify-content space-between
 </style>
